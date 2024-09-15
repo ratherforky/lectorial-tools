bae00d539031520b5d1e682e6f19cd3bb504a01d
ratherforky <jf16688@bristol.ac.uk>
2024-09-15 02:12:18 +0100

    preparing flake.nix for AWS deployment

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     inputs = {
         # Here you can adjust the IHP version of your project
         # You can find new releases at https://github.com/digitallyinduced/ihp/releases
-        ihp.url = "github:digitallyinduced/ihp/v1.1";
+        ihp.url = "github:digitallyinduced/ihp/v1.3";
         nixpkgs.follows = "ihp/nixpkgs";
         flake-parts.follows = "ihp/flake-parts";
         devenv.follows = "ihp/devenv";
@@ -37,17 +37,46 @@
                 };
             };
 
-            flake.nixosConfigurations."lectorial"  = nixpkgs.lib.nixosSystem {
+            # flake.nixosConfigurations."lectorial"  = nixpkgs.lib.nixosSystem {
+            #   system = "x86_64-linux";
+            #   specialArgs = inputs // {
+            #     environment = "production";
+            #     ihp-migrate = self.packages.x86_64-linux.migrate;
+            #     ihpApp = self.packages.x86_64-linux.default;
+            #   };
+            #   modules = [
+            #     ./nixos/configuration.nix
+            #   ];
+            # };
+            flake.nixosConfigurations."ihp-app" = nixpkgs.lib.nixosSystem {
               system = "x86_64-linux";
-              specialArgs = inputs // {
-                environment = "production";
-                ihp-migrate = self.packages.x86_64-linux.migrate;
-                ihpApp = self.packages.x86_64-linux.default;
-              };
+              specialArgs = inputs;
               modules = [
-                ./nixos/configuration.nix
+                  "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+                  ihp.nixosModules.appWithPostgres
+                  ({ ... }: {
+                      security.acme.defaults.email = "me@example.com";
+
+                      services.ihp = {
+                          # domain = "myihpapp.com";
+                          migrations = ./Application/Migration;
+                          schema = ./Application/Schema.sql;
+                          fixtures = ./Application/Fixtures.sql;
+                          sessionSecret = "xxx";
+                      };
+
+                      # Add swap to avoid running out of memory during builds
+                      # Useful if your server have less than 4GB memory
+                      swapDevices = [ { device = "/swapfile"; size = 8192; } ];
+
+                      # This should reflect the nixos version from the NixOS AMI initally installed
+                      # After the initial install, it should not be changed. Otherwise e.g. the postgres
+                      # server might need a manual data migration if NixOS changes the default postgres version
+                      system.stateVersion = "24.05";
+                  })
               ];
-            };
+          };
+
 
         };
 }
