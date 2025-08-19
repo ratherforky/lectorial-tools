4c99b358676f3a10412561547e6427c03f96b428
ratherforky <jf16688@bristol.ac.uk>
2025-08-19 02:53:33 +0100

    update logger config

--- Config/Config.hs
+++ Config/Config.hs
@@ -4,7 +4,16 @@ import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
 
+import qualified IHP.Log as Log
+import IHP.Log.Types
+
 config :: ConfigBuilder
 config = do
     option Development
     option (AppHostname "localhost")
+    logger <- liftIO $ newLogger def {
+        level = Debug,
+        formatter = withTimeFormatter,
+        destination = File "Log/production.log" (SizeRotate (Bytes (4 * 1024 * 1024)) 7) defaultBufSize
+      }
+    option logger
